b73caf5fb57aca28cd218808e54e6e69abe62bb9
Kim-Yeon-Jun <106429088+Kim-Yeon-Jun@users.noreply.github.com>
2024-12-16 11:46:39 +0900

    feat : fibo by binet

--- fibo.py
+++ fibo.py
@@ -1,8 +1,16 @@
-def fibo(num):
-    if num < 2 :
-        return num
-    else:
-        return fibo(num-1) + fibo(num-2)
+ def fibo(num):
+
+#   if num < 2 :
+#       return num
+#   else:
+#       return fibo(num-1) + fibo(num-2)
+#
+# print(fibo(5))
+
+    root_5 = 5 ** 0.5
+    phi = ((1+root_5)/2)
+    
+    a = ((phi**num) - ((-phi)**-num))/root_5
+    return round(a)
 
 print(fibo(5))
-
